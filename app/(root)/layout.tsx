9dec9a76df4ce9033bec725c122e3265e58b781d
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-05-08 23:21:20 +0530

    ui fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default function PageLayout({
   return (
     <>
       <Navbar />
-      <main className="flex min-h-[92vh] justify-center items-center flex-col gap-3 border-x relative wrapper">
+      <main className="flex min-h-[92vh] justify-center items-center flex-col gap-3 border-x relative wrapper w-full">
         {children}
         <Plus className="hidden lg:block absolute top-0 left-0 text-border -translate-x-1/2 -translate-y-1/2" />
         <Plus className="hidden lg:block absolute top-0 right-0 text-border translate-x-1/2 -translate-y-1/2" />
